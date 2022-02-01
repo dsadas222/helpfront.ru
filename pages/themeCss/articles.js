d06fab594c545b19d70cbf7e675b1108c38df7ce
Defolto <defoltmaks@gmail.com>
2022-02-01 09:09:12 +0300

    Поляков, про position

--- pages/themeCss/articles.js
+++ pages/themeCss/articles.js
@@ -1,5 +1,13 @@
 export const ARTICLES = [
-    
+    {
+        author: "Поляков А.",
+        data: '31.01.2022',
+        title: "Position",
+        description: "Все значения position",
+        directory: 'position',
+        hardLevel: 35,
+        likes: 0
+    }
 ]
 
 export const ARTICLES_CSS_LENGTH = ARTICLES.length;
